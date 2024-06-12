a2aa8ffa0666504bcf6ddfc748e7f99f95ef662a
Musarrat khan <161501864+Musarratkha@users.noreply.github.com>
2024-06-12 14:02:05 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,67 @@
+// Assignment 1
+//Miles-to-Kilometers converter (Operators)
+//* Create a variable named miles and assign a numeric value representing distance in miles.
+//*calculate the equivalent distance in Kilometers using the conversion factor (1 mile = 1.60934 Kilometers). You can achieve this by multiplying miles by 1.60934.
+//*Store the converted distance in kilometers in a variable named Kilometers.
+//*Use console.log to print a message in the following format:
+// The distance of 130 kms is equal to 209.2142 miles
+
+// let miles: number= 10;
+// let convert_into_km:number = miles*1.60934
+// let kilometer = convert_into_km
+// console.log(` There is ${kilometer} kilometer in ${miles} miles`)
+
+//Assignment 2
+//Evaluating a number game:
+
+
+
+
+//Assignment 4
+// function
+//* Set up two different variables with different values.
+//* Call a function with the variables as arguments and Output the result using console.log.
+//*Create a second call to the function with two more numbers as arguments.
+
+// let variable1: number = 4;
+// let variable2: number = 6;
+// function addNumbers() {
+//     let variables = 4 + 6
+//     console.log(variables)
+// }
+// addNumbers()
+// function addNumbers() {
+//     let variables = 7 + 8
+//     console.log(variables)
+// }
+// addNumbers()
+
+
+// assignment 6
+// let myFunction =function(value){
+//     return function(){
+//         return value;
+//     }  
+// }(1);
+// console.log(myFunction());
+
+// let myFunction =function(x){
+//     return function(y){
+//         return x+y;
+//     };  
+// };
+// let addFive = myFunction(5);
+
+// console.log(addFive(3));
+// console.log(addFive(4));
+
+// let myFunction =function(x){
+//     return function(y){
+//         return x+y;
+//     };  
+// };
+// let addFive = myFunction(5);
+
+// console.log(addFive(3));
+// console.log(addFive(4));
+
